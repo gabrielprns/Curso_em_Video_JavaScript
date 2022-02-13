8ebafa8c7cc16a9980700f7e8986d28eabc814ae
gabrielprns <gabrielpedrornn@outlook.com>
2022-02-12 23:36:55 -0300

    Project

---
+++ Aula_04/back.js
@@ -0,0 +1,13 @@
+//Exercício 1 ao 3
+
+/*var n1 = Number.parseFloat(prompt("Digite um número: "));
+var n2 = Number.parseFloat(prompt("Digite um número: "));
+var res = n1 + n2 ;
+alert(`O resultado encontrado será de : ${res}`);*/
+
+//Exercício 4
+
+var nome = prompt("Digite seu nome: ");
+document.writeln(`<p> Seu nome é <strong>${nome}</strong> e possui ${nome.length} letras. </p> `);
+document.writeln(`<p> Seu nome em Maiúsculas é <strong> ${nome.toUpperCase()}. </strong> </p> `);
+document.writeln(`<p> Seu nome em Minúsculas é <strong> ${nome.toLowerCase()}. </strong> </p> `);
