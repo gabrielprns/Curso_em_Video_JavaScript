df7b49ef43d8ea9c980018329d773e4caea2c496
gabrielprns <gabrielpedrornn@outlook.com>
2022-02-15 00:09:38 -0300

    Project

--- Aula_09/back.js
+++ Aula_09/back.js
@@ -1,3 +1,7 @@
 //window.document.write(window.document.URL);
-var p1 = window.document.getElementsByTagName("p")[0];
+var p1 = window.document.getElementById("result");
+var p2 = window.document.getElementById("Click");
+var p3 = window.document.querySelector("p.Learn");
 document.write(p1.innerHTML);
+p2.style.background = "green" ;
+p3.style.background = "blue";
