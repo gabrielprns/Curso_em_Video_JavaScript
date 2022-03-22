2f27f690438faf023f3d68787c3a45ceaf57e70a
gabrielprns <gabrielpedrornn@outlook.com>
2022-03-21 23:45:41 -0300

    Project

---
+++ Aula_excs_06/modelo/back.js
@@ -0,0 +1,8 @@
+function tabuada(){
+  let num = Number(document.getElementById("txtn").value);
+  let tab = document.getElementById("seltab");
+
+  if(num == 0){
+    window.alert("Por favor digite um número");
+  }
+}
