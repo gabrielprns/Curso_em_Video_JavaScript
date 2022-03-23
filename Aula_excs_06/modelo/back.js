6bbdc38261ae74a34634789dbcc69290653edea3
gabrielprns <gabrielpedrornn@outlook.com>
2022-03-23 17:28:06 -0300

    Project

--- Aula_excs_06/modelo/back.js
+++ Aula_excs_06/modelo/back.js
@@ -4,5 +4,13 @@ function tabuada(){
 
   if(num == 0){
     window.alert("Por favor digite um número");
+  }else{
+    tab.innerHTML = ``;
+    for(var c = 1; c<=10; c++){
+      let item = document.createElement("option");
+      item.text = `${num} x ${c} = ${num*c}`;
+      item.value = `tab${c}`;
+      tab.appendChild(item);
+    }
   }
 }
