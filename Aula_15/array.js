b6902f9bd5338f9f4567b876d106ceec26d3c099
gabrielprns <gabrielpedrornn@outlook.com>
2022-04-04 01:05:52 -0300

    Project

--- Aula_15/array.js
+++ Aula_15/array.js
@@ -1,2 +1,9 @@
 let num = [4,9,7] ;
+num.push(1);
+num.sort();
+console.log(`O vetor tem ${num.length} posições`);
 console.log(`o vetor é ${num}`);
+
+for(let pos=0; pos<num.length; pos++){
+  console.log(`A posição ${pos} tem o valor ${num[pos]}`);
+}
---
+++ Aula_15/index.js
@@ -0,0 +1,3 @@
+let num = [4,9,7] ;
+let pos = num.indexOf(4);
+console.log(`O valor 4 está ná posição ${pos}`)
